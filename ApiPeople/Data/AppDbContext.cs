ae0353477dfb4e51a3728ac913f61b75b13dfa7c
Roberto <a2020113431@estudiantes.upsa.edu.bo>
2021-10-11 19:07:55 -0400

    Agregar archivos de proyecto.

---
+++ ApiPeople/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiPeople.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeople.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }  //creando una prob DbSet<modelo>
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
